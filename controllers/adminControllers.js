b3f96267b191202b17f79f13baa20b32ed1af2da
AlexDrozd <shuradrozd@mail.ru>
2017-03-15 18:10:21 +0300

    sixth commit

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,5 +1,6 @@
 angular.module("sportsStoreAdmin")
 .constant("authUrl", "http://localhost:5500/users/login")
+.constant("ordersUrl", "http://localhost:5500/orders")
 .controller("authCtrl", function($scope, $http, $location, authUrl) {
 
 	$scope.authenticate = function (user, pass) {
@@ -34,4 +35,31 @@ angular.module("sportsStoreAdmin")
 	$scope.getScreen = function() {
 		return $scope.current == "Products" ? "/views/adminProducts.html" : "/views/adminOrders.html";
 	};
-});
+})
+
+.controller("ordersCtrl", function($scope, $http, ordersUrl) {
+	
+	$http.get(ordersUrl, {withCredentials: true})
+	.then(function(response) {
+		$scope.orders = response.data;
+	})
+
+	.catch(function(response, status) {
+		$scope.error = response.status;
+	})	
+
+	$scope.selectedOrder;
+
+	$scope.selectOrder = function(order) {
+		$scope.selectedOrder = order;
+	}
+
+	$scope.calcTotal = function(order) {
+		var total = 0;
+
+		for (var i = 0; i < order.products.length; i++) {
+			total += order.products[i].count * order.products[i].price;
+		}
+		return total;
+	} 
+});	
